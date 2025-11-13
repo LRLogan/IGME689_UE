440d08d58def1db9d07bc4079e4a76317491117b
Logan <lrl2777@g.rit.edu>
2025-11-12 19:20:41 -0500

    Nuked proj again

--- UnrealEngineProj/IGME689/Source/IGME689/IGME689.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "IGME689.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IGME689, "IGME689" );
